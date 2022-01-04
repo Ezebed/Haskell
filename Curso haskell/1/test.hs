548dc0906995ba442026a6cc1659886644e80467
Ezebed <obedvillegas.02@gmail.com>
2022-01-04 16:19:26 -0400

    adicion de codigo a test en 1

--- Curso haskell/1/test.hs
+++ Curso haskell/1/test.hs
@@ -1,8 +1,21 @@
 module Test where
 
-
+-- division entera de x entre y
 division :: Int -> Int -> Int
 division x y = x `div` y 
 
+-- eleva al cuadrado un valor entero x
 cuadrado :: Int -> Int
 cuadrado x = x * x
+
+-- genera el doble del valor x
+doble :: Int -> Int
+doble x = x * 2
+
+-- genera el valor cuadrado del valor de x
+cuad :: Int -> Int
+cuad x = doble (doble x)
+
+-- genera el triple del valor de x
+triple :: Int -> Int
+triple x = x * 3
